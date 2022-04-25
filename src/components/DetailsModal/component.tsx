a86dc79fcfa760d000ac7873cae88eb37b5728df
mudziok <mudziok@gmail.com>
2022-04-25 13:29:34 +0200

    Implement Detail Modal

--- src/components/DetailsModal/component.tsx
+++ src/components/DetailsModal/component.tsx
@@ -1,14 +1,24 @@
 import React, { FC } from 'react';
+import { Item } from '../AddNewRecordModal';
 
 import { Modal } from '../Modal';
 import { Container } from './styles';
 
-export const DetailsModal: FC = () => {
+interface DetailsModalProps {
+  product: Item
+}
+
+export const DetailsModal: FC<DetailsModalProps> = ({product}) => {
+  const {name, description, price} = product;
 
   return (
     <Modal>
-      <Container>
-        Task #4
+      <Container className='bg-white p-2 w-full md:w-96'>
+        <span className='block text-3xl font-bold'>{name}</span>
+        <span className='block text-xl'>{price} $</span>
+        <p className='mt-10'>
+          {description}
+        </p>
       </Container>
     </Modal> 
   )
