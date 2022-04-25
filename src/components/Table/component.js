4ba02b3bf64b486d40297551db228a467311a25d
Samuel <91400290+sam-kmn@users.noreply.github.com>
2022-04-25 13:41:30 +0200

    add context

--- src/components/Table/component.js
+++ src/components/Table/component.js
@@ -1,82 +1,23 @@
 
 import 'bootstrap/dist/css/bootstrap.css'
-import { useState } from 'react'
-import DeleteItem from '../DeleteItem/DeleteItem'
+// import { useState } from 'react'
+import { useItems } from '../../contexts/Items'
+// import DeleteItem from '../DeleteItem/DeleteItem'
 export const Table = () => {
 
-  const [test, setTest] = useState([
-    {
-      id: 1,
-      name: 'Foobar',
-      description: 'Lorem ipsum',
-      price: 22,
-      weight: 30
-    },
-    {
-      id: 2,
-      name: 'Foobar',
-      description: 'Lorem ipsum',
-      price: 64,
-      weight: 12
-    },
-    {
-      id: 3,
-      name: 'Foobar',
-      description: 'Lorem ipsum',
-      price: 43,
-      weight: 20
-    },
-    {
-      id: 4,
-      name: 'Foobar',
-      description: 'Lorem ipsum',
-      price: 12,
-      weight: 3
-    },
-    {
-      id: 6,
-      name: 'Foobar',
-      description: 'Lorem ipsum',
-      price: 2,
-      weight: 50
-    },
-    {
-      id: 7,
-      name: 'Foobar',
-      description: 'Lorem ipsum',
-      price: 23,
-      weight: 34
-    },
-    {
-      id: 8,
-      name: 'Foobar',
-      description: 'Lorem ipsum',
-      price: 52,
-      weight: 3
-    },
-    {
-      id: 9,
-      name: 'Foobar',
-      description: 'Lorem ipsum',
-      price: 26,
-      weight: 3
-    },
 
-
-  ]) 
-
+  const [items, setItems] = useItems()
   
-
 
   return (
     <div className='container my-5'>
       <div className="row row-cols-5 gap-4 justify-content-center">
-        {test.map( item => (
+        {items.map( item => (
           <div onClick={ () => console.log('click at item')} key={item.id} className='col border rounded shadow p-3' style={{'cursor': 'pointer'}}>
              <header className='fw-bold h4'>{item.name}</header> 
              <div>{item.description}</div> 
              <div>{item.weight}</div>
-             <DeleteItem id={item.id} test={test} setTest={setTest}/>
+             {/* <DeleteItem id={item.id} test={test} setTest={setTest}/> */}
              <div className='mt-4'>Price: <span className='fw-bold'>{'$'}{item.price}</span> </div> 
 
           </div>
---
+++ src/contexts/Items.jsx
@@ -0,0 +1,89 @@
+import { createContext, useContext, useState } from "react";
+
+
+const ItemsContext = createContext()
+
+
+export const ItemsProvider = ({children}) => {
+
+  const initialState = [
+    {
+      id: 1,
+      name: 'Foobar',
+      description: 'Lorem ipsum',
+      price: 22,
+      weight: 30
+    },
+    {
+      id: 2,
+      name: 'Foobar',
+      description: 'Lorem ipsum',
+      price: 64,
+      weight: 12
+    },
+    {
+      id: 3,
+      name: 'Foobar',
+      description: 'Lorem ipsum',
+      price: 43,
+      weight: 20
+    },
+    {
+      id: 4,
+      name: 'Foobar',
+      description: 'Lorem ipsum',
+      price: 12,
+      weight: 3
+    },
+    {
+      id: 6,
+      name: 'Foobar',
+      description: 'Lorem ipsum',
+      price: 2,
+      weight: 50
+    },
+    {
+      id: 7,
+      name: 'Foobar',
+      description: 'Lorem ipsum',
+      price: 23,
+      weight: 34
+    },
+    {
+      id: 8,
+      name: 'Foobar',
+      description: 'Lorem ipsum',
+      price: 52,
+      weight: 3
+    },
+    {
+      id: 9,
+      name: 'Foobar',
+      description: 'Lorem ipsum',
+      price: 26,
+      weight: 3
+    },
+
+
+  ]
+  const [items, setItems] = useState(initialState)
+  // const addToItems = (item) => setItems([...items, item])
+  // const removeFromItems = (item) => setItems(items.filter( i => i !== item))
+
+  return (
+    <>
+      <ItemsContext.Provider value={[items, setItems]}>
+        {children}
+      </ItemsContext.Provider>
+    </>
+  )
+}
+
+
+export const useItems = () => {
+  const context = useContext(ItemsContext);
+  if (context === undefined) {
+    throw new Error("Context must be used within a Provider");
+  }
+  return context;
+}
