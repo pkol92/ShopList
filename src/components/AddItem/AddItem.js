474b3d739cfa5a9420396f5b834b333a3c1b5ada
Dawidpios <dawid.pios00@gmail.com>
2022-04-25 12:51:06 +0200

    AddItem component

---
+++ src/components/AddItem/AddItem.js
@@ -0,0 +1,69 @@
+import {useState, useEffect} from 'react';
+import Button from './Button';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const AddItem = () => {
+    const [item , setItem] = useState([])
+    const [itemName, setItemName] = useState("")
+    const [itemDescription, setItemDescription] = useState("")
+    const [itemWeight, setitemWeight] = useState("")
+    const [itemPrice, setItemPrice] = useState("")
+
+    function handleAddItemButton(e){
+        e.preventDefault()
+
+        setItem((prevState)=>{
+            return [...prevState, 
+            {
+                id:uuidv4(),
+                name: itemName,
+                description: itemDescription,
+                price: itemPrice,
+                weight: itemWeight,
+            }]
+        })
+
+
+    }
+
+    useEffect(()=>{
+
+        console.log(item)
+    }, [item])
+
+    return ( 
+    <>
+    <form >
+        <label>Nazwa przedmiotu</label>
+        <input 
+        type="text" 
+        value={itemName}
+        onChange={(e)=> setItemName(e.target.value)}
+        ></input>
+        <label>Opis przedmiotu</label>
+        <input 
+        type="text" 
+        value={itemDescription}
+        onChange={(e)=> setItemDescription(e.target.value)}
+        ></input>
+        <label>Cena</label>
+        <input 
+         type="number" 
+         value={itemPrice}
+         onChange={(e)=> setItemPrice(e.target.value)}
+        ></input>
+         <label>Waga</label>
+        <input 
+         type="number" 
+         value={itemWeight}
+         onChange={(e)=> setitemWeight(e.target.value)}
+        ></input>
+
+        <Button type="button" onClick={handleAddItemButton}> Dodaj item</Button>
+    </form>
+    
+    </> );
+}
+ 
+export default AddItem;
---
+++ src/components/AddItem/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Button = ({type, children, onClick}) => {
+    return ( 
+        <button
+        type={type}
+        onClick={onClick}
+        >
+            {children}
+
+        </button>
+     );
+}
+ 
+export default Button;
