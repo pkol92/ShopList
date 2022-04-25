f6dda23af04f95fe75fc8f3f5927e30635d7faee
zofiadob <z.dobrowolska24@gmail.com>
2022-04-25 14:05:56 +0200

    add modal button style

--- src/components/AddNewRecordModal/styles.ts
+++ src/components/AddNewRecordModal/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+
+button{
+    background: #40B7EA;
+    padding: .7rem 1rem;
+    border: 1px solid #40B7EA;
+    border-radius: 15px;
+    transision: background 0.7s ease;
+    color: white;
+    font-weight: bold;
+    
+    &:hover{
+        background: #FFF;
+        color: #40B7EA
+    }
+}
+`;
