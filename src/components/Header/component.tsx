6c86e47d864593c08a3b63a118fcd26c79eb446a
Szymon <sz.abramowski@gmail.com>
2022-04-25 14:22:09 +0200

    update title

--- src/components/Header/component.tsx
+++ src/components/Header/component.tsx
@@ -1,11 +1,6 @@
-import React, { FC } from 'react';
-import { StyledHeader } from './styles';
+import React, { FC } from "react";
+import { StyledHeader } from "./styles";
 
 export const Header: FC = () => {
-  return (
-    <StyledHeader>
-        Task #6
-    </StyledHeader> 
-  )
-}
-    
+  return <StyledHeader>Warehouse App</StyledHeader>;
+};
