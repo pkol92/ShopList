72d93517941aaaf95787ce9c157493c8bdb76986
Aleksander <xxrtgxx333@gmail.com>
2022-04-25 14:02:47 +0200

    merge

--- src/components/DetailsModal/component.tsx
+++ src/components/DetailsModal/component.tsx
@@ -1,15 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 
 import { Modal } from '../Modal';
 import { Container } from './styles';
 
-export const DetailsModal: FC = () => {
+interface Props {
+  name: string;
+  price: number;
+  description: string;
+  weight: number;
+  id: number;
+  closeDetailPage: () => void
+  
+}
 
+export const DetailsModal: FC<Props> = ({children, name, price, description, weight, id, closeDetailPage}) => {
+ 
   return (
     <Modal>
       <Container>
-        Task #4
+        <h1>Name: {name}</h1>
+        <h3>Id: {id}</h3>
+        <h3>Price: {price}$</h3>
+        <h3>Description: {description}</h3>
+        <h3>Weight: {weight} pounds</h3>
+        <button onClick={()=>{closeDetailPage()}}>Back</button>
       </Container>
+      
     </Modal> 
   )
 }
--- src/components/DetailsModal/styles.ts
+++ src/components/DetailsModal/styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    z-index: 9;
+    background-color: white;
+    padding: 20px;
+    border-radius: 12px;
+`;
